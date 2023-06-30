35fc4d89a2707d748eff4ed0099344331a479f1b
Walter Di Sabatino <waldisab2001@gmail.com>
2023-06-30 22:40:56 +0200

    Modifiche

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // ...definisci il tuo tema personalizzato qui...
+        primaryColor: const Color(0xFF6D77FB),
+        // Imposta il colore primario come #6D77FB
+        backgroundColor: Colors
+            .white, // Imposta il colore di sfondo del contesto come bianco
+        // ...definisci altri colori personalizzati qui...
       ),
       initialRoute: '/',
       routes: {
@@ -38,27 +42,87 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Theme.of(context)
+          .backgroundColor, // Imposta il colore di sfondo del contesto
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/welcome_background.png"),
+                // Inserisci il percorso dell'immagine di sfondo
+                fit: BoxFit.cover,
+              ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: Text('Accedi'),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/logo_welcome.png',
+                  // Inserisci il percorso dell'immagine da posizionare sopra i pulsanti
+                  width: 200,
+                ),
+                SizedBox(height: 16),
+                // Aggiunto uno spazio vuoto tra l'immagine e il testo
+                const Text(
+                  'Biblioteca Nazionale',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 16),
+                // Aggiunto uno spazio vuoto tra il testo e i pulsanti
+                SizedBox(
+                  width: 300,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      primary: const Color(
+                          0xFF6D77FB), // Imposta il colore personalizzato del pulsante come #6D77FB
+                    ),
+                    child: const Text(
+                      'Accedi',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                // Aggiunto uno spazio vuoto tra i pulsanti
+                SizedBox(
+                  width: 300,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/registration');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      primary: const Color(
+                          0xFF6D77FB), // Imposta il colore personalizzato del pulsante come #6D77FB
+                    ),
+                    child: Text(
+                      'Registrati',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/registration');
-              },
-              child: Text('Registrati'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
