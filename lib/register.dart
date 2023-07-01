28d56bcdcd5b86fc887b3f5992aa9cd195ec633d
Walter Di Sabatino <waldisab2001@gmail.com>
2023-07-01 12:10:17 +0200

    Modifiche

--- lib/register.dart
+++ lib/register.dart
@@ -1,13 +1,32 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
+import 'login.dart';
+import 'main.dart';
 
 class Register extends StatelessWidget {
   const Register({Key? key});
 
   @override
   Widget build(BuildContext context) {
+    final Color customPurpleColor = const Color(0xFF6D77FB);
+    final Color customContainerColor = const Color(0xFFECEAEA);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pagina di Registrazione'),
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_rounded),
+          color: customPurpleColor,
+          iconSize: 50.0,
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => MyApp()), // Sostituisci `MyApp` con il nome corretto della schermata principale
+            );
+          },
+        ),
+        elevation: 0,
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -15,17 +34,9 @@ class Register extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                margin: EdgeInsets.only(top: 14.0),
-                child: Image.asset(
-                  'assets/baseline_arrow_back_24.png',
-                  width: 50.0,
-                  height: 50.0,
-                ),
-              ),
               SizedBox(height: 10.0),
               Text(
-                'Hello_reg_to_star',
+                'Hello! Register to get started',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 34.0,
@@ -33,78 +44,101 @@ class Register extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 204.0),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  prefixIcon: Icon(Icons.email),
-                  contentPadding: EdgeInsets.all(8.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+              SizedBox(height: 100.0),
+              Container(
+                decoration: BoxDecoration(
+                  color: customContainerColor,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                    border: InputBorder.none,
                   ),
-                  filled: true,
-                  fillColor: Colors.grey[300],
+                  obscureText: true,
                 ),
               ),
               SizedBox(height: 20.0),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  prefixIcon: Icon(Icons.lock),
-                  contentPadding: EdgeInsets.all(8.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[300],
+              Container(
+                decoration: BoxDecoration(
+                  color: customContainerColor,
+                  borderRadius: BorderRadius.circular(30),
                 ),
-                obscureText: true,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                    border: InputBorder.none,
+                  ),
+                  obscureText: true,
+                ),
               ),
               SizedBox(height: 20.0),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: 'Confirm Password',
-                  prefixIcon: Icon(Icons.lock),
-                  contentPadding: EdgeInsets.all(8.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[300],
+              Container(
+                decoration: BoxDecoration(
+                  color: customContainerColor,
+                  borderRadius: BorderRadius.circular(30),
                 ),
-                obscureText: true,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                    border: InputBorder.none,
+                  ),
+                  obscureText: true,
+                ),
               ),
               SizedBox(height: 22.0),
               ElevatedButton(
-                onPressed: () {},
-                child: Text('Register'),
+                onPressed: () {
+                  //Navigator.push(context, MaterialPageRoute(builder: (context) => BookList()));
+                  // o utilizza il metodo pushNamed se hai configurato la rotta "bookList" in main.dart
+                  // Navigator.pushNamed(context, '/bookList');
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.deepPurple,
+                  primary: customPurpleColor,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
+                    borderRadius: BorderRadius.circular(30),
                   ),
                   minimumSize: Size(double.infinity, 60.0),
                 ),
               ),
               SizedBox(height: 8.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Already registered? ',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 14.0,
-                    ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: 'Already registered? ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14.0,
                   ),
-                  Text(
-                    'Login now',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 14.0,
+                  children: [
+                    TextSpan(
+                      text: 'Login now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 14.0,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Login()),
+                          );
+                        },
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ],
           ),
