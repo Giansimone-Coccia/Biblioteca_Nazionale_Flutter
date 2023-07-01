f1ded9848294471b8745ee04795c7e4a7ee43083
Walter Di Sabatino <waldisab2001@gmail.com>
2023-07-01 12:36:04 +0200

    Struttura migliore

--- lib/login.dart
+++ lib/login.dart
@@ -9,153 +9,197 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Definisci il nuovo colore
-    final Color customPurpleColor = const Color(0xFF6D77FB);
-
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(30.0),
-            child: Card(
-              elevation: 20.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const Text(
-                      'Login',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Color(0xFF6D77FB), // Utilizza il nuovo colore
-                        fontSize: 36.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 40.0),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        border: Border.all(
-                          color: customPurpleColor, // Utilizza il nuovo colore
-                        ),
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Email',
-                          hintStyle: TextStyle(
-                            color: customPurpleColor, // Utilizza il nuovo colore
-                          ),
-                          prefixIcon: Icon(
-                            Icons.person,
-                            color: customPurpleColor, // Utilizza il nuovo colore
-                          ),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        border: Border.all(
-                          color: customPurpleColor, // Utilizza il nuovo colore
-                        ),
-                      ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Password',
-                          hintStyle: TextStyle(
-                            color: customPurpleColor, // Utilizza il nuovo colore
-                          ),
-                          prefixIcon: Icon(
-                            Icons.lock,
-                            color: customPurpleColor, // Utilizza il nuovo colore
-                          ),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                          border: InputBorder.none,
-                        ),
-                        obscureText: true,
-                      ),
-                    ),
-                    const SizedBox(height: 30.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/homepage');
-                      //  Navigator.push(context, MaterialPageRoute(builder: (context) => BookList()));
-                        // o utilizza il metodo pushNamed se hai configurato la rotta "bookList" in main.dart
-                        // Navigator.pushNamed(context, '/bookList');
-                      },
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: customPurpleColor, // Utilizza il nuovo colore
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        minimumSize: Size(double.infinity, 60.0),
-                      ),
-                    ),
-                    SizedBox(height: 12.0),
-                    const SizedBox(height: 12.0),
-                    const Text(
-                      'Login with',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14.0,
-                      ),
-                    ),
-                    SizedBox(height: 4.0),
-                    Image.asset(
-                      'assets/images/google_icon.png',
-                      width: 100.0,
-                      height: 50.0,
-                    ),
-                    SizedBox(height: 4.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Not yet registered? ',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14.0,
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => Register()));
-                            // o utilizza il metodo pushNamed se hai configurato la rotta "register" in main.dart
-                            // Navigator.pushNamed(context, '/register');
-                          },
-                          child: Text(
-                            'Register now',
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 14.0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 12.0),
-                  ],
-                ),
-              ),
-            ),
+            child: LoginCard(),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class LoginCard extends StatelessWidget {
+  const LoginCard({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 20.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            LoginTitle(),
+            const SizedBox(height: 40.0),
+            InputField(
+              hintText: 'Email',
+              prefixIcon: Icons.person,
+            ),
+            const SizedBox(height: 20.0),
+            InputField(
+              hintText: 'Password',
+              prefixIcon: Icons.lock,
+              obscureText: true,
+            ),
+            const SizedBox(height: 30.0),
+            LoginButton(onPressed: () {
+              Navigator.pushNamed(context, '/homepage');
+            }),
+            SizedBox(height: 12.0),
+            const SizedBox(height: 12.0),
+            const LoginText(
+              text: 'Login with',
+              fontSize: 14.0,
+              color: Colors.grey,
+            ),
+            SizedBox(height: 4.0),
+            Image.asset(
+              'assets/images/google_icon.png',
+              width: 100.0,
+              height: 50.0,
+            ),
+            SizedBox(height: 4.0),
+            LoginText(
+              text: 'Not yet registered? ',
+              fontSize: 14.0,
+              color: Colors.black,
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Register()));
+              },
+              child: LoginText(
+                text: 'Register now',
+                fontSize: 14.0,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 12.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginTitle extends StatelessWidget {
+  const LoginTitle({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    const Color customPurpleColor = Color(0xFF6D77FB);
+
+    return const Text(
+      'Login',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: customPurpleColor,
+        fontSize: 36.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class InputField extends StatelessWidget {
+  final String hintText;
+  final IconData prefixIcon;
+  final bool obscureText;
+
+  const InputField({
+    required this.hintText,
+    required this.prefixIcon,
+    this.obscureText = false,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Color customPurpleColor = const Color(0xFF6D77FB);
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        border: Border.all(
+          color: customPurpleColor,
+        ),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: customPurpleColor,
+          ),
+          prefixIcon: Icon(
+            prefixIcon,
+            color: customPurpleColor,
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+          border: InputBorder.none,
+        ),
+        obscureText: obscureText,
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const LoginButton({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Color customPurpleColor = const Color(0xFF6D77FB);
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        'Login',
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: customPurpleColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        minimumSize: Size(double.infinity, 60.0),
+      ),
+    );
+  }
+}
+
+class LoginText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final Color color;
+
+  const LoginText({
+    required this.text,
+    required this.fontSize,
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
       ),
     );
   }
--- lib/register.dart
+++ lib/register.dart
@@ -9,141 +9,175 @@ class Register extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Color customPurpleColor = const Color(0xFF6D77FB);
-    final Color customContainerColor = const Color(0xFFECEAEA);
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_rounded),
-          color: customPurpleColor,
-          iconSize: 50.0,
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => MyApp()), // Sostituisci `MyApp` con il nome corretto della schermata principale
-            );
-          },
-        ),
-        elevation: 0,
-      ),
+      appBar: CustomAppBar(),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(height: 10.0),
-              Text(
-                'Hello! Register to get started',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 34.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 100.0),
-              Container(
-                decoration: BoxDecoration(
-                  color: customContainerColor,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                    border: InputBorder.none,
-                  ),
-                  obscureText: true,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                  color: customContainerColor,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                    border: InputBorder.none,
-                  ),
-                  obscureText: true,
-                ),
-              ),
-              SizedBox(height: 20.0),
-              Container(
-                decoration: BoxDecoration(
-                  color: customContainerColor,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Confirm Password',
-                    contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-                    border: InputBorder.none,
-                  ),
-                  obscureText: true,
-                ),
-              ),
-              SizedBox(height: 22.0),
-              ElevatedButton(
-                onPressed: () {
-                  //Navigator.push(context, MaterialPageRoute(builder: (context) => BookList()));
-                  // o utilizza il metodo pushNamed se hai configurato la rotta "bookList" in main.dart
-                  // Navigator.pushNamed(context, '/bookList');
-                },
-                child: Text(
-                  'Register',
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: customPurpleColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  minimumSize: Size(double.infinity, 60.0),
-                ),
-              ),
-              SizedBox(height: 8.0),
-              RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  text: 'Already registered? ',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14.0,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: 'Login now',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 14.0,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Login()),
-                          );
-                        },
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          child: RegisterForm(),
         ),
       ),
     );
   }
 }
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    final Color customPurpleColor = const Color(0xFF6D77FB);
+
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back_rounded),
+        color: customPurpleColor,
+        iconSize: 50.0,
+        onPressed: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => MyApp()),
+          );
+        },
+      ),
+      elevation: 0,
+    );
+  }
+}
+class RegisterForm extends StatelessWidget {
+  final Color customPurpleColor = const Color(0xFF6D77FB);
+  final Color customContainerColor = const Color(0xFFECEAEA);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(height: 10.0),
+        Text(
+          'Hello! Register to get started',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 34.0,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 100.0),
+        CustomTextField(
+          hintText: 'Email',
+          obscureText: true,
+        ),
+        SizedBox(height: 20.0),
+        CustomTextField(
+          hintText: 'Password',
+          obscureText: true,
+        ),
+        SizedBox(height: 20.0),
+        CustomTextField(
+          hintText: 'Confirm Password',
+          obscureText: true,
+        ),
+        SizedBox(height: 22.0),
+        RegisterButton(),
+        SizedBox(height: 8.0),
+        LoginLink(),
+      ],
+    );
+  }
+}
+
+class RegisterButton extends StatelessWidget {
+  final Color customPurpleColor = const Color(0xFF6D77FB);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Register()), // Sostituisci `BookList` con la schermata a cui vuoi navigare dopo la registrazione
+        );
+      },
+      child: Text(
+        'Register',
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        primary: customPurpleColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        minimumSize: Size(double.infinity, 60.0),
+      ),
+    );
+  }
+}
+
+
+class LoginLink extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        text: 'Already registered? ',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 14.0,
+        ),
+        children: [
+          TextSpan(
+            text: 'Login now',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 14.0,
+            ),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Login()),
+                );
+              },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final bool obscureText;
+
+  const CustomTextField({
+    required this.hintText,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Color customContainerColor = const Color(0xFFECEAEA);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: customContainerColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+          border: InputBorder.none,
+        ),
+        obscureText: obscureText,
+      ),
+    );
+  }
+}
+
