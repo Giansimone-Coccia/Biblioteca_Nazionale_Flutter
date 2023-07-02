c4d04f77d6eeb8a5dd580fe07f08c08dc0a90c55
lucabellantee <s1097955@studenti.univpm.it>
2023-07-02 16:21:44 +0200

    Ulteriori modifiche su bookDelete + ottimizzazioni

--- lib/widgets/bookDelete.dart
+++ lib/widgets/bookDelete.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'bookInfo.dart';
+
 final Color customPurpleColor = const Color(0xFF6D77FB);
 
 class BookDeletePage extends StatelessWidget {
@@ -27,7 +29,8 @@ class BookDeletePage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              BookHeader(image: this.image, title: this.title, authors: this.authors),
+              BookHeader(
+                  image: this.image, title: this.title, authors: this.authors),
               SizedBox(height: 16.0),
               BookInfo(description: this.description),
               SizedBox(height: 16.0),
@@ -40,79 +43,7 @@ class BookDeletePage extends StatelessWidget {
   }
 }
 
-class BookHeader extends StatelessWidget {
-  final String image;
-  final String title;
-  final String authors;
 
-  const BookHeader({
-    required this.image,
-    required this.title,
-    required this.authors,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.network(
-              image,
-              width: 200.0,
-              height: 300.0,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(height: 16.0),
-            Center(
-              child: Text(
-                title,
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(height: 8.0),
-            Center(
-              child: Text(
-                authors,
-                style: TextStyle(fontSize: 18.0),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class BookInfo extends StatelessWidget {
-  final String description;
-
-  const BookInfo({required this.description});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Text(
-                'About the Book',
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(height: 8.0),
-            Text(description),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 class DeleteBookButton extends StatelessWidget {
   @override
@@ -124,7 +55,8 @@ class DeleteBookButton extends StatelessWidget {
           builder: (BuildContext context) {
             return AlertDialog(
               title: Text('Confirmation'),
-              content: Text('Are you sure to remove the book from the booked list?'),
+              content:
+                  Text('Are you sure to remove the book from the booked list?'),
               actions: <Widget>[
                 TextButton(
                   child: Text('No'),
@@ -147,7 +79,6 @@ class DeleteBookButton extends StatelessWidget {
           },
         );
       },
-
       style: ElevatedButton.styleFrom(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(30.0),
@@ -157,7 +88,7 @@ class DeleteBookButton extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
         child: Text(
-          'Delete booking',
+          'Delete Book',
           style: TextStyle(fontSize: 18.0),
         ),
       ),
@@ -166,6 +97,8 @@ class DeleteBookButton extends StatelessWidget {
 }
 
 
+
+/*
 void main() {
   runApp(BookDetailsApp());
 }
@@ -187,7 +120,6 @@ class BookDetailsApp extends StatelessWidget {
     );
   }
 }
-
-
+ */
 
 
